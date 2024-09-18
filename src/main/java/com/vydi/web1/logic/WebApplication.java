1765e325aef0b82d4bcaa737f4f8bb684134c425
vydibot <harrol11111@gmail.com>
2024-09-18 17:46:08 -0500

    First commit

---
+++ src/main/java/com/vydi/web1/logic/WebApplication.java
@@ -0,0 +1,14 @@
+package com.vydi.web1.logic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+
+
+    }
+}
---
+++ src/main/java/com/vydi/web1/presentacion/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.vydi.web1.presentacion.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping(value = "/homeland", method = RequestMethod.GET)
+    public String indexController(Model model) {
+        model.addAttribute("xd", "Hola");
+
+        return "index";
+    }
+}
